24868e26f0f7050b31f7e99306369cd3ca24f6ae
Juuso Issakainen <7767433+juusoi@users.noreply.github.com>
2023-07-11 13:59:15 +0300

    version 0.2

--- src/sudoku_checker.py
+++ src/sudoku_checker.py
@@ -1,25 +1,27 @@
 def is_valid_sudoku(board):
-    # Check rows
     for row in board:
-        if not is_valid_set(row):
+        if not is_valid_unit(row):
             return False
 
-    # Check columns
     for col in range(9):
         column = [board[row][col] for row in range(9)]
-        if not is_valid_set(column):
+        if not is_valid_unit(column):
             return False
 
-    # Check 3x3 squares
-    for row in range(0, 9, 3):
-        for col in range(0, 9, 3):
-            square = [board[r][c] for r in range(row, row + 3) for c in range(col, col + 3)]
-            if not is_valid_set(square):
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            square = [board[row][col] for row in range(i, i + 3) for col in range(j, j + 3)]
+            if not is_valid_unit(square):
                 return False
 
     return True
 
 
-def is_valid_set(nums):
-    nums = [n for n in nums if n != "."]
-    return len(nums) == len(set(nums))
+def is_valid_unit(unit):
+    seen = set()
+    for cell in unit:
+        if cell != ".":
+            if cell in seen or not cell.isdigit() or int(cell) < 1 or int(cell) > 9:
+                return False
+            seen.add(cell)
+    return True
